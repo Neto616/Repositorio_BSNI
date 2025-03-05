85410aa9e60dfc2fe2889fe28af6366b98bb0fb6
Neto-IBS <140271677+Neto616@users.noreply.github.com>
2025-03-05 12:36:17 -0600

    FEATURE: Practica 1 utilizar componentes en react

---
+++ trabajos_react/my-app/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import MyApp from './Button.js';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <MyApp />
+      </header>
+    </div>
+  );
+}
+
+export default App;
