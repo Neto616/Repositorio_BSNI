7a82f4f624ae52f40c3c6813a2590a360bf5aca2
Neto-IBS <140271677+Neto616@users.noreply.github.com>
2025-03-22 20:16:38 -0600

    Se añadio el evento de manera correcta para la obtencion de los datos

--- trabajos_react/calculadora/src/App.js
+++ trabajos_react/calculadora/src/App.js
@@ -1,36 +1,42 @@
 import { useState } from "react"
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const [valor, setValue] = useState("0")
+  const [valor, setValor] = useState("0");
+
+  const onClickFunction = (event) => {
+    if (valor === "0") setValor(event.target.value);
+    else
+      setValor(valor.concat(event.target.value))
+  }
+
   return (
-    <div class="calculator">
+    <div className="calculator">
         <div id="display" class="display">{valor}</div>
-        <div class="buttons">
-            <button class="btn clear">C</button>
-            <button class="btn">(</button>
-            <button class="btn">)</button>
-            <button class="btn">/</button>
+        <div className="buttons">
+            <button className="btn clear">C</button>
+            <button className="btn" value={"("} onClick={(e)=> onClickFunction(e)}>(</button>
+            <button className="btn" value={")"} onClick={(e)=> onClickFunction(e)}>)</button>
+            <button className="btn" value={"/"} onClick={(e)=> onClickFunction(e)}>/</button>
 
-            <button class="btn">7</button>
-            <button class="btn">8</button>
-            <button class="btn">9</button>
-            <button class="btn">*</button>
+            <button className="btn" value={"7"} onClick={(e)=> onClickFunction(e)}>7</button>
+            <button className="btn" value={"8"} onClick={(e)=> onClickFunction(e)}>8</button>
+            <button className="btn" value={"9"} onClick={(e)=> onClickFunction(e)}>9</button>
+            <button className="btn" value={"*"} onClick={(e)=> onClickFunction(e)}>*</button>
 
-            <button class="btn">4</button>
-            <button class="btn">5</button>
-            <button class="btn">6</button>
-            <button class="btn">-</button>
+            <button className="btn" value={"4"} onClick={(e)=> onClickFunction(e)}>4</button>
+            <button className="btn" value={"5"} onClick={(e)=> onClickFunction(e)}>5</button>
+            <button className="btn" value={"6"} onClick={(e)=> onClickFunction(e)}>6</button>
+            <button className="btn" value={"-"} onClick={(e)=> onClickFunction(e)}>-</button>
 
-            <button class="btn">1</button>
-            <button class="btn">2</button>
-            <button class="btn">3</button>
-            <button class="btn">+</button>
+            <button className="btn" value={"1"} onClick={(e)=> onClickFunction(e)}>1</button>
+            <button className="btn" value={"2"} onClick={(e)=> onClickFunction(e)}>2</button>
+            <button className="btn" value={"3"} onClick={(e)=> onClickFunction(e)}>3</button>
+            <button className="btn" value={"+"} onClick={(e)=> onClickFunction(e)}>+</button>
 
-            <button class="btn zero" colspan="2">0</button>
-            <button class="btn">.</button>
-            <button class="btn equal" rowspan="2">=</button>
+            <button className="btn zero" colspan="2" value={"0"} onClick={(e)=> onClickFunction(e)}>0</button>
+            <button className="btn" value={"."} onClick={(e)=> onClickFunction(e)}>.</button>
+            <button className="btn equal" rowspan="2">=</button>
         </div>
     </div>
   );
